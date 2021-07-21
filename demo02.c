3361c413cd676eb822219e989ad4a33d3cec7f08
DearGG <72794249+DearGG@users.noreply.github.com>
2021-07-21 20:38:51 +0800

    Add files via upload

---
+++ demo02.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int num = 100;
+
+int demo02() {
+	//定义变量
+	int num = 10;
+	int num1;
+	//int num2 = 0;
+	printf("num:%d\n", num);
+
+	//使用scanf函数
+	scanf("%d",&num1);
+	printf("num1:%d\n", num1);
+
+	return 0;
+}
